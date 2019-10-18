3fea8b5d4522ac9ce8f4797bac6dcc97867a11f2
Gabriel <gbr98>
2019-10-18 10:02:46 -0300

    geração das famílias planares

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#import gurobipy as gb
+import gurobipy as gb
 import numpy as np
 from itertools import permutations
 #from utils import *
@@ -7,24 +7,41 @@ from itertools import permutations
 def main():
 
 	instanceFileName = 'exemple'
-	nTerminalNodes = 5
+	nTerminalNodes = 3
 
 	# Carregar estrutura do grafo
 	#g = read_graph(instanceFileName)
 	# ---
 
-	perm = list(permutations(np.linspace(1,nTerminalNodes,nTerminalNodes)))
-	base = fam = np.linspace(1,nTerminalNodes,nTerminalNodes)
+	perm = list(permutations(np.linspace(1,nTerminalNodes-1,nTerminalNodes-1)))
+	base = list(np.linspace(1,nTerminalNodes,nTerminalNodes).astype(np.int))
 
 	for i in range(len(perm)):
 		# Para cada família laminar, deve-se gerar e resolver
 		# um modelo de programação linear
 
 		# Toma família laminar
-		
+		P = []
+		S = [base]
+		sliced = [base]
+		sliceCounter = nTerminalNodes-1
+		for j in range(0,nTerminalNodes-1):
+			cutPoint = int(perm[i][j])
+			print(cutPoint)
+			swap = []
+			for s in sliced:
+				if cutPoint in s and len(s) > 1:
+					cut = s.index(cutPoint)+1
+					swap.append(s[:cut])
+					swap.append(s[cut:])
+					S.append(s[:cut])
+					S.append(s[cut:])
+					P.append((s[:cut],s[cut:]))
+			sliced = swap.copy()
+			#print(sliced)
+		print(S,P)
 		# ---
-		a = 5
-		#model = gb.Model('delivery')
 
+		model = gb.Model('delivery')
 		
 main()
