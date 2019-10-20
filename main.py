f6de8f69b2c76af1ec52d2d76998cd401da51811
Gabriel Oliveira <gabrieloliveira@ice.ufjf.br>
2019-10-20 17:09:57 -0300

    v1

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 from itertools import permutations
 from utils import *
 from igraph import *
+from random import random
 
 def main():
 
@@ -13,9 +14,44 @@ def main():
 
 	# Carregar estrutura do grafo
 	g = read_graph(instanceFileName)
-	print(g)
-	nEdges = 10
-	nVertices = 8
+	nEdges = len(g.es)
+	nVertices = len(g.vs)
+	A = []
+	W = list(g.es['weight'])
+	for i in range(nEdges):
+		print(g.es[i].tuple)
+	print(W)
+	ex = list(g.es.select(_source=1))
+	for e in ex:
+		print(e.tuple)
+
+	delta_p = []
+	delta_m = []
+	for i in range(nVertices):
+		delta_p.append([])
+		delta_m.append([])
+	for i in range(nEdges):
+		delta_p[g.es[i].source-1].append(i)
+		delta_m[g.es[i].target-1].append(i)
+	
+	# Selecionar terminais
+	remainingNodes = np.linspace(1,nVertices,nVertices)
+	terminalNodes = []
+	startNode = remainingNodes[int(np.floor(random()*nTerminalNodes))]
+	remainingNodes = np.delete(remainingNodes, startNode-1)
+	for i in range(nTerminalNodes):
+		posNode = int(np.floor(random()*remainingNodes.size))
+		terminalNodes.append(remainingNodes[posNode])
+		remainingNodes = np.delete(remainingNodes, posNode)
+	print(startNode, terminalNodes)
+	'''
+	print(nVertices, nEdges)
+	for i in range(nVertices):
+		line = ""
+		for j in range(nVertices):
+			line += str(g[i,j])+" "
+		print(line)
+	'''
 	# ---
 
 	perm = list(permutations(np.linspace(1,nTerminalNodes-1,nTerminalNodes-1)))
@@ -32,7 +68,7 @@ def main():
 		sliceCounter = nTerminalNodes-1
 		for j in range(0,nTerminalNodes-1):
 			cutPoint = int(perm[i][j])
-			print(cutPoint)
+			#print(cutPoint)
 			swap = []
 			for s in sliced:
 				if cutPoint in s and len(s) > 1:
@@ -50,12 +86,45 @@ def main():
 		model = gb.Model('delivery')
 
 		# Create variables
-		f = addVars((nEdges, len(S)), vtype=GRB.BINARY)
-		yhat, ybar = addVars((nVertices, len(S)), vtype=GRB.BINARY),addVars((nVertices, len(S)), vtype=GRB.BINARY)
-		w = addVars((nVertices, len(P)), vtype=GRB.BINARY)
+		f = model.addVars((nEdges, len(S)), vtype=GRB.BINARY)
+		yhat, ybar = model.addVars((nVertices, len(S)), vtype=GRB.BINARY),addVars((nVertices, len(S)), vtype=GRB.BINARY)
+		w = model.addVars((nVertices, len(P)), vtype=GRB.BINARY)
 
 		# Set objective
-	    m.setObjective(, GRB.MINIMIZE)
+		model.setObjective(sum(W[edge]*f.sum(edge,'*') for edge in range(len(W))), GRB.MINIMIZE)
+
+	    # Add Constrainsts
+		for i in range(nVertices):
+			for s in range(len(S)):
+				model.addConstr(f.sum(delta_p[i],s)-f.sum(delta_m[i],s) == yhat[i,s]-ybar[i,s]) #(1)
+		for s in range(len(S)):
+			if len(S[s]) >= 2:
+				for p in range(len(P)):
+					if list(P[p][0]+P[p][1]) == list(S[s]):
+						for i in range(nVertices):
+							model.addConstr(w[i,p] == ybar[i,s]) #(2)
+		for p in range(len(P)):
+			for s in range(len(p)):
+				for i in range(nVertices):
+					model.addConstr(w[i,p] == yhat[i,s]) #(3)
+		for p in range(len(P)):
+			model.addConstr(w.sum('*',p) == 1) #(4)
+
+
+		model.addConstr(yhat[startNode,0] == 1) #(5)
+
+		for i in range(nVertices):
+			if i+1 != startNode:
+				model.addConstr(yhat[i,0] == 0) #(6)
+
+		for b in base:
+			unitIndex = S.index([b])
+			model.addConstr(ybar[terminalNodes[b-1],unitIndex] == 1) #(7)
+			for i in range(nVertices):
+				if i != terminalNodes[b-1]:
+					model.addConstr(ybar[i,unitIndex] == 1) #(8)
+
+		model.optimize()
 
 		'''
 		# Create variables
