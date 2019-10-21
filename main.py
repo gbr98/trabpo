7f06f48ae5c9d28a65bc72a74281b730229c90a3
Gabriel Oliveira <gabrieloliveira@ice.ufjf.br>
2019-10-21 00:43:32 -0300

    .

--- main.py
+++ main.py
@@ -158,7 +158,7 @@ def main():
 	print(bestValue)
 	f = bestSolution.getVars()
 	print(dir(f[0]))
-	print(f[0].getAttr('x'))
+	print(f[0].'X')
 	#for i in range(nEdges):
 	#	for j in range(len(S)):
 	#		print(f[i,j].X)
